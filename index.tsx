094fcdfbee9365158abc0ede25e51c5f5a43210a
Azis Nurhamid <azisnurhamid@gmail.com>
2026-02-03 22:42:49 +0700

    Remove PWA

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,19 +5,6 @@ import App from './App';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
-}
-
-// Service Worker Registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
 }
 
 const root = ReactDOM.createRoot(rootElement);
